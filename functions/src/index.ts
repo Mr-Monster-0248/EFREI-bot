8419d139e0e2984ad61d5823285943722f8393d7
Thibault Lepez <thibault.lepez@efrei.net>
2020-01-25 00:08:25 +0100

    added index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,13 @@
 import * as functions from 'firebase-functions';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//Web scraping dependencies
+import fetch from 'node-fetch';
+import * as cheerio from 'cheerio';
+
+// Google Assistant dependancies
+import { dialogflow, SimpleResponse, BasicCard, Button, Image } from 'actions-on-google';
+
+const app = dialogflow({ debug: true });
+
+// Capture an intent
+
