5fd839f59ebeb6071ecbdab7d2bf7f2aa30d7795
Thibault LEPEZ <thibault.lepez@efrei.net>
2020-05-16 16:44:51 +0200

    reformat function

--- functions/src/scrap/events.ts
+++ functions/src/scrap/events.ts
@@ -1,46 +1,50 @@
 import axios from "axios";
 
 export interface EventCard {
-	readonly title: string,
-	readonly date: string,
-	readonly beginTime: string,
-	readonly endTime: string
+  readonly title: string;
+  readonly date: string;
+  readonly beginTime: string;
+  readonly endTime: string;
 }
 
 export async function getEvent(type: string = "") {
-	return new Promise<EventCard[]>(async (resolve, reject) => {
-		let url: string;
-		switch (type) {
-			case 'JPO': 
-				url = "https://www.myefrei.fr/api/public/wp/queries/events?eventType=Portes%20ouvertes&sort=ASC&limit=3";
-				break;
-			case 'Prepa bac':
-				url = "https://www.myefrei.fr/api/public/wp/queries/events?eventType=Préparation%20Bac&sort=ASC&limit=3";
-				break;
-			case 'Projette toi':
-				url = "https://www.myefrei.fr/api/public/wp/queries/events?eventType=Journ%C3%A9e%20d%C3%A9couverte%20Projette-toi%20@%20Efrei%20Paris&sort=ASC&limit=3";
-				break;
-			default:
-				url = "https://www.myefrei.fr/api/public/wp/queries/events?sort=ASC&limit=3";
-				break;
-		}
+  return new Promise<EventCard[]>(async (resolve, reject) => {
+    let url: string;
+    switch (type) {
+      case "JPO":
+        url =
+          "https://www.myefrei.fr/api/public/wp/queries/events?eventType=Portes%20ouvertes&sort=ASC";
+        break;
+      case "Prepa bac":
+        url =
+          "https://www.myefrei.fr/api/public/wp/queries/events?eventType=Préparation%20Bac&sort=ASC&limit=3";
+        break;
+      case "Projette toi":
+        url =
+          "https://www.myefrei.fr/api/public/wp/queries/events?eventType=Journ%C3%A9e%20d%C3%A9couverte%20Projette-toi%20@%20Efrei%20Paris&sort=ASC&limit=3";
+        break;
+      default:
+        url =
+          "https://www.myefrei.fr/api/public/wp/queries/events?sort=ASC&limit=3";
+        break;
+    }
 
-		try {
-			let response = await axios.get(url);
+    try {
+      let response = await axios.get(url);
 
-			let repTab: EventCard[] = [];
-			response.data.rows.forEach((elem: any, i: number) => {
-				repTab[i] = {
-					title: elem.eventName,
-					date: elem.eventbeginDate,
-					beginTime: elem.eventBeginTime,
-					endTime: elem.eventEndTime
-				};
-			});
+      let repTab: EventCard[] = [];
+      response.data.rows.forEach((elem: any, i: number) => {
+        repTab[i] = {
+          title: elem.eventName,
+          date: elem.eventBeginDate,
+          beginTime: elem.eventBeginTime,
+          endTime: elem.eventEndTime,
+        };
+      });
 
-			resolve(repTab);
-		} catch (error) {
-			reject(error);
-		}
-	});
+      resolve(repTab);
+    } catch (error) {
+      reject(error);
+    }
+  });
 }
--- functions/src/scrap/majeures.ts
+++ functions/src/scrap/majeures.ts
@@ -1,36 +1,40 @@
 import * as request from "request";
-import * as cheerio from 'cheerio';
+import * as cheerio from "cheerio";
 
 export interface MajorCard {
-    readonly img: string,
-    readonly title: string,
-    readonly descript: string,
-    readonly link: string
+  readonly img: string;
+  readonly title: string;
+  readonly descript: string;
+  readonly link: string;
 }
 
+/**
+ * Function that scrap all the 'majeures' on efrei.fr
+ * @returns An array of MajorCard
+ */
 export async function getMajeures() {
-    return new Promise<MajorCard[]>((resolve, reject) => {
-        const url =
-            "https://www.efrei.fr/programme-grande-ecole/le-cycle-ingenieur/majeures-specialisation/";
-        let tab: MajorCard[] = [];
-        request(url, (error, response, html) => {
-            if (!error && response.statusCode == 200) {
-                const $ = cheerio.load(html);
+  return new Promise<MajorCard[]>((resolve, reject) => {
+    const url =
+      "https://www.efrei.fr/programme-grande-ecole/le-cycle-ingenieur/majeures-specialisation/";
+    let tab: MajorCard[] = [];
+    request(url, (error, response, html) => {
+      if (!error && response.statusCode == 200) {
+        const $ = cheerio.load(html);
 
-                $(".element-anime").each((i, elem) => {
-                    let text: string = <string>$(elem).find(".imgbg").attr("style");
-                    tab[i] = {
-                        img: text.substring(16, text.length - 3),
-                        descript: $(elem).find("p").text(),
-                        title: $(elem).find("h3").text(),
-                        link: <string> $(elem).find("a").attr("href")
-                    }
-                });
-            } else {
-                reject("Could not resolve url");
-            }
-
-            resolve(tab);
+        $(".element-anime").each((i, elem) => {
+          let text: string = <string>$(elem).find(".imgbg").attr("style");
+          tab[i] = {
+            img: text.substring(16, text.length - 3),
+            descript: $(elem).find("p").text(),
+            title: $(elem).find("h3").text(),
+            link: <string>$(elem).find("a").attr("href"),
+          };
         });
+      } else {
+        reject("Could not resolve url");
+      }
+
+      resolve(tab);
     });
+  });
 }
